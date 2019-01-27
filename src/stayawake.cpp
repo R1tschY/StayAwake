9c12b194d8a6d1c9c44024759948c1e9e6b7040d
R1tschY <r1tschy@yahoo.de>
2019-01-27 17:55:13 +0100

    Updated copyright notice

--- src/stayawake.cpp
+++ src/stayawake.cpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of StayAwake.
  *
- *  Copyright (C) 2010, 2015  Richard Liebscher <r1tschy@yahoo.de>
+ *  Copyright (C) 2019  Richard Liebscher <r1tschy@yahoo.de>
  *
  *  StayAwake is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
